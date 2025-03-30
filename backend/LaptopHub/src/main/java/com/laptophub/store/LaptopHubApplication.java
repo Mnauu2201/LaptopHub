4dc28ed817873f92a3de6d9639fca61813d3e05a
ANON-n <thodibar2201@gmail.com>
2025-03-30 15:36:39 +0700

    folder backend

---
+++ backend/LaptopHub/src/main/java/com/laptophub/store/LaptopHubApplication.java
@@ -0,0 +1,13 @@
+package com.laptophub.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopHubApplication.class, args);
+	}
+
+}
---
+++ backend/LaptopHub/src/test/java/com/laptophub/store/LaptopHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptophub.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
