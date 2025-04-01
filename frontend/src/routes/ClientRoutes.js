85f746b40b107ccb943ef562dce4affc1979ea8b
ducanhvoi <142959422+ducanhvoi@users.noreply.github.com>
2025-04-01 17:57:12 +0700

    chinh sua cac routes va them folder

---
+++ frontend/src/routes/ClientRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+function ClientRoutes() {
+  return (
+    <Switch>
+    </Switch>
+  );
+}
+
+export default ClientRoutes;
